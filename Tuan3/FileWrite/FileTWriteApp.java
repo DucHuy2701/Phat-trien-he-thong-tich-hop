12dcde73bbf2074183ec84074f0f7db47fe4c92b
Huy Le <leduchuy271@gmail.com>
2021-10-05 13:47:13 +0700

    up

--- Tuan3/FileWrite/FileTWriteApp.java
+++ Tuan3/FileWrite/FileTWriteApp.java
@@ -5,19 +5,19 @@ import java.io.FileOutputStream;
 public class FileTWriteApp {
     public static void main(String[] args){
         FileOutputStream fos1 = null;
-        FileOutputStream fos2 = null;
-        FileOutputStream fos3 = null;
+        //FileOutputStream fos2 = null;
+        //FileOutputStream fos3 = null;
         try {
-            fos1 = new FileOutputStream("twrite1.txt");
-            fos2 = new FileOutputStream("twrite2.txt");
-            fos3 = new FileOutputStream("twrite3.txt");
+            fos1 = new FileOutputStream("./txt/twrite1.txt");
+            //fos2 = new FileOutputStream("/txt/twrite2.txt");
+            //fos3 = new FileOutputStream("/txt/twrite3.txt");
         } catch (Exception e) {
             System.out.println(e);
         }
 
         FileTWrite t1 = new FileTWrite(fos1);
-        FileTWrite t2 = new FileTWrite(fos2);
-        FileTWrite t3 = new FileTWrite(fos3);
+        //FileTWrite t2 = new FileTWrite(fos2);
+        //FileTWrite t3 = new FileTWrite(fos3);
 
         t1.start();
         //t2.start();
