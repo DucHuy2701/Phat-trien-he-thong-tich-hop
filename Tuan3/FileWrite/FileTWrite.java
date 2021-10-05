7d2858c29eaad8b690905912842b38d1b37a437c
Huy Le <leduchuy271@gmail.com>
2021-10-05 14:16:22 +0700

    14:16 commit

--- Tuan3/FileWrite/FileTWrite.java
+++ Tuan3/FileWrite/FileTWrite.java
@@ -2,8 +2,6 @@ package Tuan3.FileWrite;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Random;
-import java.util.Scanner;
 
 public class FileTWrite extends Thread{
     FileOutputStream fos;
@@ -17,16 +15,11 @@ public class FileTWrite extends Thread{
     }
 
     public void run(){
-        Scanner sc = new Scanner(System.in);
-        Random randInt = new Random();
-        System.out.println(Thread.currentThread().getName()+" => N: ");
-        int n = sc.nextInt();
-        sc.close();
-        for(int i=0; i<n; i++){
+        double rand = Math.random();
+        System.out.println(Thread.currentThread().getName()+" running...");
+        for(int i=0; i<10; i++){
             try {
-                String out = randInt.toString();
-                byte b[] = out.getBytes();
-                this.fos.write(b);
+                this.fos.write((int) rand);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -36,6 +29,6 @@ public class FileTWrite extends Thread{
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Done.."+Thread.currentThread().getName());
+        System.out.println("Done "+Thread.currentThread().getName());
     }
 }
--- Tuan3/FileWrite/FileTWriteApp.java
+++ Tuan3/FileWrite/FileTWriteApp.java
@@ -1,26 +1,21 @@
 package Tuan3.FileWrite;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class FileTWriteApp {
-    public static void main(String[] args){
-        FileOutputStream fos1 = null;
-        //FileOutputStream fos2 = null;
-        //FileOutputStream fos3 = null;
-        try {
-            fos1 = new FileOutputStream("./txt/twrite1.txt");
-            //fos2 = new FileOutputStream("/txt/twrite2.txt");
-            //fos3 = new FileOutputStream("/txt/twrite3.txt");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public static void main(String[] args) throws FileNotFoundException{
+
+        FileOutputStream fos1 = new FileOutputStream("Tuan3\\FileWrite/txt/twrite1.txt");
+        FileOutputStream fos2 = new FileOutputStream("Tuan3\\FileWrite/txt/twrite2.txt");
+        FileOutputStream fos3 = new FileOutputStream("Tuan3\\FileWrite/txt/twrite3.txt");
 
         FileTWrite t1 = new FileTWrite(fos1);
-        //FileTWrite t2 = new FileTWrite(fos2);
-        //FileTWrite t3 = new FileTWrite(fos3);
+        FileTWrite t2 = new FileTWrite(fos2);
+        FileTWrite t3 = new FileTWrite(fos3);
 
         t1.start();
-        //t2.start();
-        //t3.start();}
+        t2.start();
+        t3.start();
     }
 }
